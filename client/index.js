6c768b706cb3e8c67e76191ac158eda65d779c88
hansen1416 <badapplesweetie@gmail.com>
2024-12-16 14:02:14 +0800

    1

--- client/index.js
+++ client/index.js
@@ -1,14 +1,14 @@
 const { Connection, PublicKey, Program } = require('@solana/web3.js');
 
 // Replace with your program ID
-const programId = new PublicKey('Fg6PaFkwSMi2XwCnkh3t7w3NStqR6USo4kU5WAzp5uPB3');
+const programId = new PublicKey('CcXDtgNex3qFycGqSMDzY1dAMrdqLQN5h1RwNkv3PSvF');
 
 // Replace with your RPC endpoint
-const connection = new Connection('https://api.devnet.solana.com');
+const connection = new Connection('http://127.0.0.1:8899');
 
 const main = async () => {
   // ... your Web3.js logic her
-  const publicKey = new PublicKey('YourPublicKeyHere');
+  const publicKey = new PublicKey('CcXDtgNex3qFycGqSMDzY1dAMrdqLQN5h1RwNkv3PSvF');
   const accountInfo = await connection.getAccountInfo(publicKey);
 
   if (accountInfo) {
